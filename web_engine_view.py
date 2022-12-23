d43517ebe5a546c5df41fc8427ec11a8103d9e95
lowetu <xialowetu@gmail.com>
2022-12-23 22:41:37 +0800

    重写resizeEvent以实现webview跟随界面大小变化而变化

--- web_engine_view.py
+++ web_engine_view.py
@@ -1,26 +1,32 @@
 import os
+import pathlib
+import time
 
-from PySide6.QtCore import QUrl, QRect
+from PySide6.QtCore import QUrl
 from PySide6.QtGui import QStandardItemModel, QStandardItem, QGuiApplication
-from PySide6.QtWebEngineWidgets import QWebEngineView
-from PySide6.QtWidgets import QMainWindow, QGridLayout, QApplication
+from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtGui import QResizeEvent
 
 from ui.main import Ui_MainWindow
 
+MY_MD = None
+
 
 class MainWindow(QMainWindow):
+    md = None
+
     def __init__(self):
-        super(MainWindow,self).__init__()
+        super(MainWindow, self).__init__()
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setWindowTitle(u"Fly Editor--fly with you idea")
         self.browser = self.ui.index_web_view
         self.full_screen_flag = False
-        self.setGeometry(5,30,400,400)
-        self.url = os.getcwd()+'/front/html/index.html'
+        self.setGeometry(5, 30, 400, 400)
+        self.url = os.getcwd() + '/front/html/index.html'
         self.screen = QGuiApplication.primaryScreen().geometry()
-        self.resize(self.screen.width(),self.screen.height())
+        self.resize(self.screen.width(), self.screen.height())
         self.file_list = self.ui.file_list  # 文件列表栏
         self.directory_tree = self.ui.directory_tree  # 文件夹列表栏
         self.btn_twoCol = self.ui.btn_two_col
@@ -38,8 +44,17 @@ class MainWindow(QMainWindow):
         self.btn_fullScreen.clicked.connect(self.show_full_screen)
 
         self.browser.load(QUrl.fromLocalFile(self.url))
+        self.init_file()
 
-        # self.browser.resize(self.width(), self.height())
+    def init_file(self):
+        """暂存文件初始化"""
+        if os.path.exists('temp'):
+            os.remove('temp')
+
+    def resizeEvent(self, event: QResizeEvent) -> None:
+        """重写resize事件，实现webview跟随控件变化而变化"""
+        super().resizeEvent(event)
+        self.browser.resize(self.width(), self.height())
 
     def show_two_col(self):
         """
@@ -47,13 +62,9 @@ class MainWindow(QMainWindow):
         """
         self.file_list.hide()
         self.btn_twoCol.hide()
-        # self.btn_fullScreen.show()
         self.directory_tree.show()
         self.btn_threeCol.show()
-        self.browser.resize(self.width(),self.height())
-        self.browser.load(QUrl.fromLocalFile(self.url))
-
-        # self.browser.setGeometry(0, 0, self.screen.width(),self.screen.height())
+        self.browser.resize(self.width(), self.height())
 
     def show_three_col(self):
         """
@@ -65,12 +76,26 @@ class MainWindow(QMainWindow):
         self.file_list.show()
         self.directory_tree.show()
         self.btn_threeCol.hide()
-        # self.browser.setGeometry(0, 0, self.screen.width(), self.screen.height())
-        self.browser.resize(self.width(),self.height())
-        self.browser.load(QUrl.fromLocalFile(self.url))
+        self.browser.resize(self.width(), self.height())
 
+    def run_js_get_content(self, content_type):
+        """运行js，获取markdown内容"""
+        if content_type == "md":
+            return '''contentEditor.getMarkdown();'''
+        elif content_type == "html":
+            return '''contentEditor.getHTML();'''
+
+    def js_callback(self, res=None):
+        # self.browser.page().runJavaScript(
+        #     f'''alert("{res}")''')
+        print(res)
 
     def show_full_screen(self):
+        """
+        全屏显示
+        :return:
+        """
+
         if not self.full_screen_flag:
             self.btn_twoCol.hide()
             self.btn_threeCol.hide()
@@ -84,14 +109,15 @@ class MainWindow(QMainWindow):
 
             self.show_three_col()
             self.full_screen_flag = False
-            self.btn_threeCol.show()
-        self.browser.resize(self.width(),self.height())
-        self.browser.load(QUrl.fromLocalFile(self.url))
 
-
+        self.browser.resize(self.width(), self.height())
 
 
     def directory_tree_test(self):
+        """
+        测试目录树
+        :return:
+        """
         model = QStandardItemModel(self.directory_tree)
         model.setHorizontalHeaderLabels(["笔记目录"])
         item_directory_1 = QStandardItem("文件夹1")
@@ -101,9 +127,3 @@ class MainWindow(QMainWindow):
         item_directory_1.appendRow(item_file_1)
         item_directory_1.appendRow(item_file_2)
         self.directory_tree.setModel(model)
-
-
-
-
-
-
